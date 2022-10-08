a72bb91f4bbb1cd5eb158e224a8bd2e56af1cf61
JDSNX <judisonbacalso24@gmail.com>
2022-10-08 14:52:19 +0800

    read only if face detected

--- src/face/recognizer.py
+++ src/face/recognizer.py
@@ -9,6 +9,8 @@ class FaceRecognition:
     encodings='encodings.pickle'
     fourcc = cv2.VideoWriter_fourcc(*"MJPG")
     data = pickle.loads(open(encodings, "rb").read())
+    recognize = 'output/authorize'
+    unrecognize = 'output/unauthorize'
 
     def __init__(self, video_channel=0, output='output/video.avi', detection_method='hog'):
         self.output = output
@@ -31,44 +33,43 @@ class FaceRecognition:
             rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             r = frame.shape[1] / float(rgb.shape[1])
 
-            boxes = face_recognition.face_locations(rgb,
-                model=self.detection_method)
-
+            boxes = face_recognition.face_locations(rgb, model=self.detection_method)
             encodings = face_recognition.face_encodings(rgb, boxes)
+            
             names = []
+            if boxes and encodings:
+                for encoding in encodings:
+                    matches = face_recognition.compare_faces(self.data["encodings"], encoding)
+                    name = "Unknown"
 
-            for encoding in encodings:
-                matches = face_recognition.compare_faces(self.data["encodings"], encoding)
-                name = "Unknown"
+                    if True in matches:
+                        matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                        counts = {}
 
-                if True in matches:
-                    matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-                    counts = {}
+                        for i in matchedIdxs:
+                            name = self.data["names"][i]
+                            counts[name] = counts.get(name, 0) + 1
 
-                    for i in matchedIdxs:
-                        name = self.data["names"][i]
-                        counts[name] = counts.get(name, 0) + 1
+                        name = max(counts, key=counts.get)
 
-                    name = max(counts, key=counts.get)
+                    names.append(name)
 
-                names.append(name)
+                for ((top, right, bottom, left), name) in zip(boxes, names):
+                    top = int(top * r)
+                    right = int(right * r)
+                    bottom = int(bottom * r)
+                    left = int(left * r)
 
-            for ((top, right, bottom, left), name) in zip(boxes, names):
-                top = int(top * r)
-                right = int(right * r)
-                bottom = int(bottom * r)
-                left = int(left * r)
+                    detected("FR", 1)
+                    if name == "Unknown":
+                        detected("FR", 0)
+                        color = (0, 0, 255)
 
-                detected("FR", 1)
-                if name == "Unknown":
-                    detected("FR", 0)
-                    color = (0, 0, 255)
-
-                cv2.rectangle(frame, (left, top), (right, bottom),
-                    color, 2)
-                y = top - 15 if top - 15 > 15 else top + 15
-                cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.75, color, 2)
+                    cv2.rectangle(frame, (left, top), (right, bottom),
+                        color, 2)
+                    y = top - 15 if top - 15 > 15 else top + 15
+                    cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.75, color, 2)
 
             if writer is None and self.output is not None:
                 writer = cv2.VideoWriter(self.output, self.fourcc, 20,
