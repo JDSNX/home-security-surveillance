efaf7d9e211a9b0eb3b02903c6f1ba331fdd1e24
JDSNX <judisonbacalso24@gmail.com>
2022-11-23 14:34:30 +0800

    added creating .env file

--- src/main-face.py
+++ src/main-face.py
@@ -5,8 +5,9 @@ if __name__ == '__main__':
 
     if args == "train":
         from face.encoder import EncodeFaces
-        from modules import get_images
-
+        from modules import get_images, create_env
+        
+        create_env()
         get_images()
         ef = EncodeFaces()
         ef.encode_faces()
@@ -16,31 +17,6 @@ if __name__ == '__main__':
         
         fr = FaceRecognition()
         fr.face_recognize()
-
-    else:
-        import cv2
-        import time
-
-        cap = cv2.VideoCapture(0)
-        pTime = 0
-        while True:
-            ret, frame = cap.read()
-
-            cTime = time.time()
-            fps = 1/(cTime-pTime)
-            pTime=cTime
-
-            if ret is False:
-                break
-
-            cv2.putText(frame, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX,
-                1, (0, 255, 2), 2)
-
-            cv2.imshow('frame', frame)
-            
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        cap.release()
-        cv2.destroyAllWindows()
     
+    else:
+        print('[ERROR] Wrong paramater passed...')
